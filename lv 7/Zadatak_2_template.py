e5670ebc67e43fd9de3abe00c0b0ec815836b4d2
LeonBenic <64423174+Leeonbenaa@users.noreply.github.com>
2023-05-08 16:10:17 +0200

    Add files via upload

--- lv 7/Zadatak_2_template.py
+++ lv 7/Zadatak_2_template.py
@@ -13,47 +13,51 @@ plt.imshow(img)
 plt.tight_layout()
 plt.show()
 
+
 # pretvori vrijednosti elemenata slike u raspon 0 do 1
 img = img.astype(np.float64) / 255
 
 # transfromiraj sliku u 2D numpy polje (jedan red su RGB komponente elementa slike)
-w, h, d = img.shape
+w,h,d = img.shape
 img_array = np.reshape(img, (w*h, d))
+
 
 # rezultatna slika
 img_array_aprox = img_array.copy()
 
-km = KMeans(n_clusters=3, init='random',
-            n_init=5, random_state=0)
+colors = np.unique(img_array_aprox, axis=0)
+print(f'{len(colors)} is the number of unique colors in this image.')
 
+n_clusters = 5
+km = KMeans(n_clusters=n_clusters, init='random', n_init=5)
 km.fit(img_array_aprox)
-labels = km.predict(img_array_aprox)
-colors = km.cluster_centers_[labels]
-img = np.reshape(colors, (w, h, d))
+colorLabels = km.predict(img_array_aprox)
+quantizedColors = km.cluster_centers_[colorLabels]
+quantizedImg = np.reshape(quantizedColors, (w,h,d))
 plt.figure()
 plt.title("Kvantizirana slika")
-plt.imshow(img)
+plt.imshow(quantizedImg)
 plt.tight_layout()
 plt.show()
 
-j = []
+# što je manji K, to slika više odudara od originala
 
-for n_clusters in range(1, 8):
-    km = KMeans(n_clusters=n_clusters, init='k-means++',
-                n_init=5, random_state=0)
-    km.fit(img_array_aprox)
-    labels = km.predict(img_array_aprox)
-    j.append(km.inertia_)
+# j = []
+# for n_clusters in range(1,10):
+#     km = KMeans(n_clusters=n_clusters, init='random', n_init=5)
+#     km.fit(img_array_aprox)
+#     colorLabels = km.predict(img_array_aprox)
+#     j.append(km.inertia_)
 
+# plt.plot(range(1,10), j)
+# plt.show()
+# lakat je K=4
 
-plt.figure()
-plt.plot(range(1, 8), j)
-plt.show()
-
-for i in range(0, 3):
-    b = labels == i
-    b = np.reshape(b, (w, h))
+for i in range(0,n_clusters):
+    boolArray = colorLabels == i
+    boolArray = np.reshape(boolArray, (w,h))
     plt.figure()
-    plt.imshow(b)
+    plt.title(f"Binarna slika {i}. grupe")
+    plt.imshow(boolArray)
     plt.tight_layout()
     plt.show()
